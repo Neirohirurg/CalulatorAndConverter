9a9e71fd90975aa4614efd8886a4fdd0bd33e1e1
nik <n.kostrukov@mail.ru>
2024-03-05 18:48:33 +0300

    update

--- Calculator/ConsoleCalculator/obj/Release/net6.0/ConsoleCalculator.AssemblyInfo.cs
+++ Calculator/ConsoleCalculator/obj/Release/net6.0/ConsoleCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9cba630751173fbf5925cb502d6440376f5ba41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69193eeaab2e7059021ba268b5d6119be07cb66")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
